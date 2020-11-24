326d2276be65bb9c83de38d741f07f065f285508
hardiksrashtasoft <74816683+hardiksrashtasoft@users.noreply.github.com>
2020-11-24 18:59:23 +0530

    Add files via upload

---
+++ Template/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+	$('.accordion').find('.accordion-toggle').click(function() {
+		$(this).next().slideToggle('600');
+		$(".accordion-content").not($(this).next()).slideUp('600');
+	});
+	$('.accordion-toggle').on('click', function() {
+		$(this).toggleClass('active').siblings().removeClass('active');
+	});
+});
+
+function toggleIcon(e) {
+    $(e.target)
+        .prev('.panel-heading')
+        .find(".more-less")
+        .toggleClass('glyphicon-plus glyphicon-minus');
+}
+$('.panel-group').on('hidden.bs.collapse', toggleIcon);
+$('.panel-group').on('shown.bs.collapse', toggleIcon);
