fea313da95bcb5a62c29f26df67aeeae5967099a
hardiksrashtasoft <74816683+hardiksrashtasoft@users.noreply.github.com>
2020-11-26 13:29:44 +0530

    Add files via upload

--- Template1/js/main.js
+++ Template1/js/main.js
@@ -45,3 +45,80 @@ $(document).ready(function(){
 		$(this).addClass("active-time4");
 			});
 });
+
+
+$(document).ready(function(){
+  $(document).on('click','.bookBtn1', function(e){
+    $(".hideS1").show();
+    $(".expandHide1").show();
+    $(".expandPrice1").hide();
+    $(this).css("background-color", "#00E600");
+    $(this).addClass("bookBtn2")
+    $(this).removeClass("bookBtn1");
+  });
+  $(document).on('click','.bookBtn2', function(e){
+    $(".hideS1").hide();
+    $(".expandHide1").hide();
+    $(".expandPrice1").show();
+    $(this).css("background-color", "#6C757D");
+    $(this).addClass("bookBtn1")
+    $(this).removeClass("bookBtn2");
+  });
+});
+
+$(document).ready(function(){
+  $(document).on('click','.bookBtn3', function(e){
+    $(".hideS2").show();
+    $(".expandHide2").show();
+    $(".expandPrice2").hide();
+    $(this).css("background-color", "#00E600");
+    $(this).addClass("bookBtn4")
+    $(this).removeClass("bookBtn3");
+  });
+  $(document).on('click','.bookBtn4', function(e){
+    $(".hideS2").hide();
+    $(".expandHide2").hide();
+    $(".expandPrice2").show();
+    $(this).css("background-color", "#6C757D");
+    $(this).addClass("bookBtn3")
+    $(this).removeClass("bookBtn4");
+  });
+});
+
+$(document).ready(function(){
+  $(document).on('click','.bookBtn5', function(e){
+    $(".hideS3").show();
+    $(".expandHide3").show();
+    $(".expandPrice3").hide();
+    $(this).css("background-color", "#00E600");
+    $(this).addClass("bookBtn6")
+    $(this).removeClass("bookBtn5");
+  });
+  $(document).on('click','.bookBtn6', function(e){
+    $(".hideS3").hide();
+    $(".expandHide3").hide();
+    $(".expandPrice3").show();
+    $(this).css("background-color", "#6C757D");
+    $(this).addClass("bookBtn5")
+    $(this).removeClass("bookBtn6");
+  });
+});
+
+$(document).ready(function(){
+  $(document).on('click','.bookBtn7', function(e){
+    $(".hideS4").show();
+    $(".expandHide4").show();
+    $(".expandPrice4").hide();
+    $(this).css("background-color", "#00E600");
+    $(this).addClass("bookBtn8")
+    $(this).removeClass("bookBtn7");
+  });
+  $(document).on('click','.bookBtn8', function(e){
+    $(".hideS4").hide();
+    $(".expandHide4").hide();
+    $(".expandPrice4").show();
+    $(this).css("background-color", "#6C757D");
+    $(this).addClass("bookBtn7")
+    $(this).removeClass("bookBtn8");
+  });
+});
